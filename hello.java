56b2aa8d3c36ae21cefe0e264a05d3035b4af166
ovadyamoshe <37333126+ovadyamoshe@users.noreply.github.com>
2022-02-20 11:33:17 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
-public class Hello {
-  public static void main(String[] asgs){
-    //print "hello in the console"
-    System.out.printIn("Hello world");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
